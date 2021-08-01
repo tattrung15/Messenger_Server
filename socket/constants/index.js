e3534a8aba508f7e16458aca7cc7eafecbbf6f6a
Tất Trung <buitrungt@gmail.com>
2021-08-01 17:46:04 +0700

    add socket documentation

--- socket/constants/index.js
+++ socket/constants/index.js
@@ -5,6 +5,7 @@ module.exports.SocketEvent = {
   SV_SEND_CURR_USER: "server-send-current-user",
   SV_SEND_USERS_ONLINE: "server-send-users-online",
   SV_SEND_CONVERSATIONS: "server-send-conversations",
+  SV_SEND_CURR_CONVERSATION: "server-send-current-conversation",
   SV_SEND_MESSAGE: "server-send-message",
   SV_SEND_MESSAGE_TO_AUTHOR: "server-send-message-to-author",
   CLIENT_JOIN_ROOM: "client-join-room",
@@ -16,4 +17,5 @@ module.exports.SocketErrorMessage = {
   CREATE_CONVERSATION: "Create new conversation",
   CLIENT_DISCONNECT: "Client disconnect",
   SEND_MESSAGE: "Send message",
+  JOIN_ROOM: "Join room",
 };
--- socket/listeners/joinRoom.js
+++ socket/listeners/joinRoom.js
@@ -1,5 +1,27 @@
+const Conversation = require("../../models/Conversation.model");
+
+const { SocketEvent, SocketErrorMessage } = require("../constants");
+
 module.exports = (io, socket) => async (data) => {
-  const { roomId } = data;
-  socket.roomId = roomId;
-  socket.join(roomId);
+  try {
+    const { roomId } = data;
+
+    const conversation = await Conversation.findById(roomId);
+
+    if (!conversation) {
+      socket.emit(SocketEvent.ERROR, {
+        message: SocketErrorMessage.JOIN_ROOM,
+        result: "Not found room",
+      });
+
+      return;
+    }
+
+    socket.roomId = roomId;
+    socket.join(roomId);
+
+    socket.emit(SocketEvent.SV_SEND_CURR_CONVERSATION, conversation);
+  } catch (error) {
+    console.log(error);
+  }
 };
