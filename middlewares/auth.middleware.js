3144939908b88c4f9ee8f673e4a7adb971ed905a
Tất Trung <buitrungt@gmail.com>
2021-08-06 14:04:44 +0700

    api get messages in conversation

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -12,9 +12,7 @@ module.exports = async (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith("Bearer ")) {
-    return res
-      .status(HttpStatus.UNAUTHORIZED)
-      .json(new ResponseEntity(HttpStatus.UNAUTHORIZED, "Unauthorized"));
+    return next(new ResponseEntity(HttpStatus.UNAUTHORIZED, "Unauthorized"));
   }
 
   const token = authorization.substring(7);
@@ -22,10 +20,17 @@ module.exports = async (req, res, next) => {
 
   const user = await User.findOne({ username });
 
+  if (!user) {
+    return next(new ResponseEntity(HttpStatus.UNAUTHORIZED, "Unauthorized"));
+  }
+
+  if (req.baseUrl === "/api/messages") {
+    req.user = user;
+    return next();
+  }
+
   if (userId !== user._id.toString()) {
-    return res
-      .status(HttpStatus.UNAUTHORIZED)
-      .json(new ResponseEntity(HttpStatus.UNAUTHORIZED, "Unauthorized"));
+    return next(new ResponseEntity(HttpStatus.FORBIDDEN, "Forbidden"));
   }
 
   req.user = user;
