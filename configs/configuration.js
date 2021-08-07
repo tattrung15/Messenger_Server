7fba7854a889b007076f2e137ec297f3ce38b800
Tất Trung <buitrungt@gmail.com>
2021-08-07 16:54:27 +0700

    socket send file

--- configs/configuration.js
+++ configs/configuration.js
@@ -1,11 +1,12 @@
 require("dotenv").config();
 
 module.exports = () => ({
-  port: process.env.PORT || 3000,
+  port: +process.env.PORT || 3000,
   database: {
     MONGO_URI:
       process.env.MONGO_URI || "mongodb://localhost:27017/realtime-chat",
   },
   JWT_SECRET: process.env.JWT_SECRET || "abcxyz",
   NODE_ENV: process.env.NODE_ENV || "development",
+  MAX_FILE_SIZE: 5242880, // 5MB
 });
--- socket/listeners/index.js
+++ socket/listeners/index.js
@@ -4,11 +4,13 @@ const getConversation = require("./getConversation");
 const createConversation = require("./createConversation");
 const leaveConversation = require("./leaveConversation");
 const sendMessage = require("./sendMessage");
+const sendFile = require("./sendFile");
 
 module.exports = {
   joinRoom,
   disconnect,
   sendMessage,
+  sendFile,
   getConversation,
   createConversation,
   leaveConversation,
